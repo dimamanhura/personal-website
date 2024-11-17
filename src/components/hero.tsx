f3496d80ffaffcd6a64d2603a2375795dd312de8
Manhura Dima <manguradima@gmail.com>
2024-11-17 18:08:28 +0200

    Add styles for hero description

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,15 +10,18 @@ const Hero = ({ meta }: HeroProps) => {
   return (
     <>
       <div className="w-full flex flex-col lg:flex-row justify-center items-center gap-4 py-6 px-6 bg-zinc-100">
-        <Avatar src={meta.avatar} className="w-40 h-40 text-large border-white border-2" />
-        <div className="flex flex-col gap-1">
+        <Avatar
+          src={meta.avatar}
+          className="w-44 h-44 text-large border-white border-2 flex-shrink-0"
+        />
+        <div className="flex flex-col gap-1 lg:max-w-[600px]">
           <h1 className="w-full text-2xl text-center md:text-4xl md:text-left">
             {meta.firstName} {meta.lastName}
           </h1>
-          <p className="w-full text-center text-xl md:text-left">
+          <p className="w-full text-center text-xl md:text-left text-zinc-600">
             {meta.title}
           </p>
-          <p className="w-full">
+          <p className="w-full text-sm text-zinc-500 text-justify">
             {meta.description}
           </p>
         </div>
