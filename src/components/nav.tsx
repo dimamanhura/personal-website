ef85d24aa206800054c5688fafeda97330c38f22
Manhura Dima <manguradima@gmail.com>
2025-09-21 15:16:09 +0300

    Admin nav with logo at the left side

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,27 +1,41 @@
 'use client';
 
+import { Fragment, useState } from "react";
+import { FaChevronDown } from "react-icons/fa";
 import { usePathname, redirect } from "next/navigation";
 import {
   NavbarMenuToggle,
+  DropdownTrigger,
   NavbarMenuItem,
   NavbarContent,
+  DropdownMenu,
+  DropdownItem,
+  NavbarBrand,
   NavbarItem,
   NavbarMenu,
+  Dropdown,
   Navbar,
+  Button,
+  Avatar,
   Link,
 } from "@nextui-org/react";
 import RouterLink from "next/link";
-import { Fragment, useState } from "react";
 import ThemeSwitcher from "./theme-switcher";
 import { Page } from "@/types/Page";
+import paths from "@/paths";
 
 interface NavProps {
   pages: Page[];
 };
 
+const MAX_LINK_TO_SHOW_IN_NAV = 4;
+
 const Nav = ({ pages }: NavProps) => {
   const pathname = usePathname();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const linksToShow: Page[] = pages.slice(0, MAX_LINK_TO_SHOW_IN_NAV);
+  const linksToShowMore: Page[] = pages.slice(MAX_LINK_TO_SHOW_IN_NAV, pages.length);
 
   const handleChangePage = (path: string) => {
     setIsMenuOpen(false);
@@ -76,16 +90,54 @@ const Nav = ({ pages }: NavProps) => {
 
   return (
     <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} className="print:hidden">
+      <NavbarBrand className="hidden lg:block">
+        <Link href={paths.home()}>
+          <Avatar src="/logo.jpg" size="sm" />
+        </Link>
+      </NavbarBrand>
+    
       <NavbarContent className="md:hidden" justify="start">
         <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+        <NavbarBrand>
+          <Link href={paths.home()}>
+            <Avatar src="/logo.jpg" size="sm" />
+          </Link>
+        </NavbarBrand>
       </NavbarContent>
     
       <NavbarContent className="hidden md:flex gap-3" justify="start">
-        {pages.map((page, index) => (
+        {linksToShow.map((page, index) => (
           <Fragment key={index}>
             {renderLink(page)}
           </Fragment>
         ))}
+        {
+          linksToShowMore.length > 0 && (
+            <Dropdown>
+              <NavbarItem>
+                <DropdownTrigger>
+                  <Button
+                    disableRipple
+                    className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                    endContent={<FaChevronDown />}
+                    radius="sm"
+                    variant="light"
+                    size="lg"
+                  >
+                    More
+                  </Button>
+                </DropdownTrigger>
+              </NavbarItem>
+              <DropdownMenu aria-label="More links" itemClasses={{ base: "gap-4" }}>
+                {linksToShowMore.map(({ title, path }) => (
+                  <DropdownItem key={path}>
+                    {renderMenuLink({ title, path })}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+          )
+        }
       </NavbarContent>
 
       <NavbarContent justify="end">
